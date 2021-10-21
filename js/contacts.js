f271212fc5dc9ff99b4da115ffe70bc66ea2dffa
NevidX <433android@mail.ru>
2021-10-21 18:00:05 +0300

    Deleted extra code

--- js/contacts.js
+++ js/contacts.js
@@ -1,9 +1,6 @@
 const contactsButton = document.querySelector('#contacts')
 const contacts = document.querySelector('.contacts__adaptive')
 const contactsArrow = document.querySelector('.services-link__arrow')
-console.log(contactsArrow);
-
-console.log(contactsButton);
 contactsButton.addEventListener('click', () => {
 	contacts.classList.toggle('contacts__adaptive-active')
 	contactsArrow.classList.toggle('services-link__arrow-active')
